88d177c880fc9167a9c63526fdc3d1b16ee68225
kordeamey <ameykorde47@gmail.com>
2024-07-31 13:32:22 +0530

    Add files via upload

---
+++ program/table/src/table/TableOf10.java
@@ -0,0 +1,8 @@
+package table;
+public class TableOf10 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("10 x " + i + " = " + (10 * i));
+        }
+    }
+}
